2ddc38e601743f5163f0842a569306016d89d012
Ashley Barr <ashley.barr@ar4s.com>
2022-03-15 01:33:25 -0600

    initial commit

---
+++ .history/svelte.config_20220315010617.js
@@ -0,0 +1,56 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import UnoCss from 'unocss/vite';
+import { extractorSvelte } from '@unocss/core';
+import { presetAttributify, presetIcons, presetUno } from 'unocss';
+
+import staticAdapter from '@sveltejs/adapter-static'
+import nodeAdapter from '@sveltejs/adapter-node'
+import multiAdapter from '@macfja/svelte-multi-adapter'
+
+
+
+export default config;
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: multiAdapter([staticAdapter(adapter()), nodeAdapter(/* ... */)]),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		// target: '#svelte',
+		vite: {
+			plugins: [
+				UnoCss({
+					theme: {
+						fontFamily: {
+							sans: 'Outfit'
+						}
+					},
+					extractors: [extractorSvelte],
+					shortcuts: [
+						{
+							layout: 'max-w-1000px mx-auto p-4'
+						}
+					],
+					presets: [
+						presetAttributify(),
+						presetUno(),
+						presetIcons({
+							extraProperties: {
+								display: 'inline-block',
+								'vertical-align': 'middle'
+							}
+						})
+					]
+				})
+			]
+		}
+	}
+};
+
+export default config;
---
+++ .history/svelte.config_20220315012249.js
@@ -0,0 +1,54 @@
+import multiAdapter from '@macfja/svelte-multi-adapter'
+import adapter from '@sveltejs/adapter-auto';
+import staticAdapter from '@sveltejs/adapter-static'
+import nodeAdapter from '@sveltejs/adapter-node'
+// import svelte-adapter-deno from 'svelte-adapter-deno';
+import preprocess from 'svelte-preprocess';
+import UnoCss from 'unocss/vite';
+import { extractorSvelte } from '@unocss/core';
+import { presetAttributify, presetIcons, presetUno } from 'unocss';
+
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: multiAdapter([staticAdapter(), nodeAdapter(/* ... */), adapter()]),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		// target: '#svelte',
+		vite: {
+			plugins: [
+				UnoCss({
+					theme: {
+						fontFamily: {
+							sans: 'Outfit'
+						}
+					},
+					extractors: [extractorSvelte],
+					shortcuts: [
+						{
+							layout: 'max-w-1000px mx-auto p-4'
+						}
+					],
+					presets: [
+						presetAttributify(),
+						presetUno(),
+						presetIcons({
+							extraProperties: {
+								display: 'inline-block',
+								'vertical-align': 'middle'
+							}
+						})
+					]
+				})
+			]
+		}
+	}
+};
+
+export default config;
